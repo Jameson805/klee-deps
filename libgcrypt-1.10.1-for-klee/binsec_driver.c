a8f71513ee393709c6960e8aba9c0a03d9fd55fa
theta-lin <theta.lin@yandex.com>
2025-09-01 16:33:06 +0800

    libgcrypt: refactor driver and Makefile, attempt to support BinSec/Rel (not working)

--- libgcrypt-1.10.1-for-klee/binsec_driver.c
+++ libgcrypt-1.10.1-for-klee/binsec_driver.c
@@ -1,21 +1,15 @@
 #include <gcrypt.h>
-#include <klee/klee.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
 #define SYM_SIZE 32
+// Allocate symbolic buffers for base, exponent, and modulus
+unsigned char base_buf[SYM_SIZE];
+unsigned char exp_buf[SYM_SIZE];
+unsigned char mod_buf[SYM_SIZE];
 
 int main() {
-    // Allocate symbolic buffers for base, exponent, and modulus
-    unsigned char base_buf[SYM_SIZE];
-    unsigned char exp_buf[SYM_SIZE];
-    unsigned char mod_buf[SYM_SIZE];
-
-    klee_make_symbolic(base_buf, sizeof(base_buf), "base");
-    klee_make_symbolic(exp_buf, sizeof(exp_buf), "exp");
-    klee_make_symbolic(mod_buf, sizeof(mod_buf), "mod");
-
     // Parse symbolic buffers into gcry_mpi_t big numbers
     gcry_mpi_t base, exp, mod, result;
     if (gcry_mpi_scan(&base, GCRYMPI_FMT_USG, base_buf, SYM_SIZE, NULL) != 0)
@@ -25,7 +19,7 @@ int main() {
     if (gcry_mpi_scan(&mod, GCRYMPI_FMT_USG, mod_buf, SYM_SIZE, NULL) != 0)
         return 4;
 
-    result = gcry_mpi_new(0);
+    result = gcry_mpi_new(SYM_SIZE * 8);
 
     // Perform modular exponentiation
     gcry_mpi_powm(result, base, exp, mod);
