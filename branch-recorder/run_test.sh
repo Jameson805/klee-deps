5c019d76ca9c37def027fd96de92dbc829988174
theta-lin <theta.lin@yandex.com>
2026-05-21 23:50:28 -0400

    Remove branch-recorder

--- branch-recorder/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")"
-clang -O0 -g -emit-llvm -c test.c -o test.bc
-llvm-dis test.bc
-opt -load ./build/libBranchRecorder.so -load-pass-plugin=./build/libBranchRecorder.so \
-    -passes=branch-recorder -whitelist=f test.bc -o test_instrumented.bc
-llvm-dis test_instrumented.bc
-lli test_instrumented.bc
